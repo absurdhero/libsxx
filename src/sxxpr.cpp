14ad3f28fcf23c24d54594ae8ee7f70cb827f700
Brandon Bremen <bbremen@gmail.com>
2016-09-02 09:07:45 -0700

    to_text for dotted pairs and lists

--- src/sxxpr.cpp
+++ src/sxxpr.cpp
@@ -18,35 +18,38 @@ std::string Sexpr::to_text() const {
 
     sexpr_variants current(value);
     while (true) {
-        if (holds_alternative<void*>(value)) {
+        if (holds_alternative<void*>(current)) {
             text += "<pointer>";
             break;
         }
-        else if (holds_alternative<sexpr_cons>(value)) {
-            sexpr_cons cons = get<sexpr_cons>(value);
+        else if (holds_alternative<sexpr_cons>(current)) {
+            sexpr_cons cons = get<sexpr_cons>(current);
             text += cons.car->to_text();
             if (!cons.cdr || cons.cdr->is_empty())
                 break;
-            text += ' ';
             current = cons.cdr->value;
+            if (holds_alternative<sexpr_cons>(current))
+                text += ' ';
+            else
+                text += " . ";
         }
-        else if (holds_alternative<bool>(value)) {
-            if (get<bool>(value))
+        else if (holds_alternative<bool>(current)) {
+            if (get<bool>(current))
                 text += "#t";
             else
                 text += "#f";
             break;
         }
-        else if (holds_alternative<double>(value)) {
-            text += std::to_string(get<double>(value));
+        else if (holds_alternative<double>(current)) {
+            text += std::to_string(get<double>(current));
             break;
         }
-        else if (holds_alternative<std::shared_ptr<std::string>>(value)) {
-            text += *get<std::shared_ptr<std::string>>(value);
+        else if (holds_alternative<std::shared_ptr<std::string>>(current)) {
+            text += *get<std::shared_ptr<std::string>>(current);
             break;
         }
-        else if (holds_alternative<int64_t>(value)) {
-            text += std::to_string(get<int64_t>(value));
+        else if (holds_alternative<int64_t>(current)) {
+            text += std::to_string(get<int64_t>(current));
             break;
         }
     }
--- test/test_sexpr.cpp
+++ test/test_sexpr.cpp
@@ -7,11 +7,44 @@ TEST(Sexpr, as_str)
   ASSERT_STREQ("foo", sexpr.as_str()->c_str());
 }
 
-TEST(Sexpr, list_size_one)
+TEST(ToText, list_size_one)
 {
 	Sexpr sexpr(std::make_shared<Sexpr>("foo"), Sexpr::empty);
 	ASSERT_EQ(*Sexpr::empty, *sexpr.cdr());
 	ASSERT_STREQ("foo", sexpr.car()->as_str()->c_str());
 	ASSERT_STREQ("foo", sexpr.car()->to_text().c_str());
 	ASSERT_STREQ("(foo)", sexpr.to_text().c_str());
+}
+
+TEST(ToText, list_size_two)
+{
+	auto two = std::make_shared<Sexpr>(std::make_shared<Sexpr>("bar"), Sexpr::empty);
+	Sexpr one(std::make_shared<Sexpr>("foo"), two);
+	ASSERT_STREQ("foo", one.car()->to_text().c_str());
+	ASSERT_STREQ("(bar)", one.cdr()->to_text().c_str());
+	ASSERT_STREQ("(foo bar)", one.to_text().c_str());
+}
+
+TEST(ToText, dotted_pair)
+{
+	Sexpr sexpr(std::make_shared<Sexpr>("foo"), std::make_shared<Sexpr>("bar"));
+	ASSERT_STREQ("(foo . bar)", sexpr.to_text().c_str());
+}
+
+
+TEST(ToText, list_size_three)
+{
+    auto three = std::make_shared<Sexpr>(std::make_shared<Sexpr>("baz"), Sexpr::empty);
+    auto two = std::make_shared<Sexpr>(std::make_shared<Sexpr>("bar"), three);
+    Sexpr one(std::make_shared<Sexpr>("foo"), two);
+    ASSERT_STREQ("(foo bar baz)", one.to_text().c_str());
+}
+
+TEST(ToText, list_size_three_dotted)
+{
+    auto two = std::make_shared<Sexpr>(
+            std::make_shared<Sexpr>("bar"),
+            std::make_shared<Sexpr>("baz"));
+    Sexpr one(std::make_shared<Sexpr>("foo"), two);
+    ASSERT_STREQ("(foo bar . baz)", one.to_text().c_str());
 }
