04636a0545164d302eec8f457b4220a2396bab12
Brandon Bremen <bbremen@gmail.com>
2016-09-08 17:47:26 -0700

    change tabs to spaces

--- src/include/sxx/sexpr.hpp
+++ src/include/sxx/sexpr.hpp
@@ -7,26 +7,26 @@
 
 namespace sxx {
 
-	class Sexpr;
+    class Sexpr;
 
-	class SexprTypeException : public std::exception {
-	public:
+    class SexprTypeException : public std::exception {
+    public:
         virtual const char* what() {
             return "type mismatch";
         }
-	};
+    };
 
-	/** Symbols are immutable strings with their own distinct type.
-	* Symbols should share their string values with each other.
-	*/
+    /** Symbols are immutable strings with their own distinct type.
+    * Symbols should share their string values with each other.
+    */
     class Symbol {
     private:
         std::shared_ptr<const std::string> value;
     public:
         Symbol(const Symbol &str) : value(str.value) {}
         Symbol(const Symbol &&str) : value(str.value) {}
-		Symbol(const std::shared_ptr<const std::string> value) : value(value) {}
-		Symbol(const std::string &str) : value(std::make_shared<const std::string>(str)) {}
+        Symbol(const std::shared_ptr<const std::string> value) : value(value) {}
+        Symbol(const std::string &str) : value(std::make_shared<const std::string>(str)) {}
         Symbol(const std::string &&str) : value(std::make_shared<const std::string>(str)) {}
 
         const char *c_str() {
@@ -37,161 +37,161 @@ namespace sxx {
             return value;
         }
 
-		bool operator==(const Symbol& rhs) const {
-			return *value == *rhs.value;
-		}
-	};
+        bool operator==(const Symbol& rhs) const {
+            return *value == *rhs.value;
+        }
+    };
 
-	/** The main s-expression value type that holds any value or a pair of values (Sexpr::pair). */
-	class Sexpr {
-	protected:
+    /** The main s-expression value type that holds any value or a pair of values (Sexpr::pair). */
+    class Sexpr {
+    protected:
 
-		any value;
+        any value;
 
-	public:
+    public:
 
-		typedef std::shared_ptr<Sexpr> ptr;
+        typedef std::shared_ptr<Sexpr> ptr;
 
-		struct pair {
-			Sexpr::ptr car;
-			Sexpr::ptr cdr;
+        struct pair {
+            Sexpr::ptr car;
+            Sexpr::ptr cdr;
 
-			pair(Sexpr::ptr car, Sexpr::ptr cdr) : car(car), cdr(cdr) {}
-			pair() : car(), cdr() {}
+            pair(Sexpr::ptr car, Sexpr::ptr cdr) : car(car), cdr(cdr) {}
+            pair() : car(), cdr() {}
 
-			pair(const pair &other) = default;
+            pair(const pair &other) = default;
 
-			bool operator==(const pair& rhs) const;
-		};
+            bool operator==(const pair& rhs) const;
+        };
 
-		static const Sexpr empty;
+        static const Sexpr empty;
 
-		Sexpr() {}
-		Sexpr(const Sexpr &other) = default;
-		Sexpr(Sexpr &&other) = default;
-		virtual ~Sexpr() = default;
+        Sexpr() {}
+        Sexpr(const Sexpr &other) = default;
+        Sexpr(Sexpr &&other) = default;
+        virtual ~Sexpr() = default;
 
-		Sexpr(const char *value) : value(std::string(value)) {}
-		Sexpr(Symbol value) : value(value) {}
-		Sexpr(std::string value) : value(value) {}
-		Sexpr(Sexpr::pair value) : value(value) {}
+        Sexpr(const char *value) : value(std::string(value)) {}
+        Sexpr(Symbol value) : value(value) {}
+        Sexpr(std::string value) : value(value) {}
+        Sexpr(Sexpr::pair value) : value(value) {}
 
-		Sexpr(const any &value) : value(value) {}
-		Sexpr(any &&value) : value(value) {}
+        Sexpr(const any &value) : value(value) {}
+        Sexpr(any &&value) : value(value) {}
 
-		bool operator==(const Sexpr &rhs) const;
+        bool operator==(const Sexpr &rhs) const;
 
         bool as_bool() const {
             return any_cast<bool>(value);
         }
 
-		pair as_pair() const {
-			return any_cast<pair>(value);
-		}
+        pair as_pair() const {
+            return any_cast<pair>(value);
+        }
 
-		std::string as_string() const {
-			return any_cast<std::string>(value);
-		}
+        std::string as_string() const {
+            return any_cast<std::string>(value);
+        }
 
-		Symbol as_symbol() const {
-			return any_cast<Symbol>(value);
-		}
+        Symbol as_symbol() const {
+            return any_cast<Symbol>(value);
+        }
 
-		bool is_empty() const {
-			return value.empty();
-		}
+        bool is_empty() const {
+            return value.empty();
+        }
 
-		bool is_bool() const {
-			return value.type() == typeid(bool);
-		}
+        bool is_bool() const {
+            return value.type() == typeid(bool);
+        }
 
-		bool is_pair() const {
-			return value.type() == typeid(pair);
-		}
+        bool is_pair() const {
+            return value.type() == typeid(pair);
+        }
 
-		bool is_string() const {
-			return value.type() == typeid(std::string);
-		}
+        bool is_string() const {
+            return value.type() == typeid(std::string);
+        }
 
-		bool is_symbol() const {
-			return value.type() == typeid(Symbol);
-		}
+        bool is_symbol() const {
+            return value.type() == typeid(Symbol);
+        }
 
-		Sexpr::ptr first() const;
+        Sexpr::ptr first() const;
 
-		Sexpr::ptr rest() const;
+        Sexpr::ptr rest() const;
 
-		void set_first(Sexpr *first);
+        void set_first(Sexpr *first);
 
-		void set_rest(Sexpr *rest);
+        void set_rest(Sexpr *rest);
 
-		std::string to_text() const;
-	};
+        std::string to_text() const;
+    };
 
 
-	// Rich Sexpr wrapper classes
+    // Rich Sexpr wrapper classes
 
-	class List {
-	public:
-		static const Sexpr::ptr null;
+    class List {
+    public:
+        static const Sexpr::ptr null;
 
-		Sexpr::ptr sexpr;
+        Sexpr::ptr sexpr;
 
-		List(Sexpr::ptr car, Sexpr::ptr cdr) : sexpr(std::make_shared<Sexpr>(Sexpr::pair(car, cdr)))
-		{
-		}
+        List(Sexpr::ptr car, Sexpr::ptr cdr) : sexpr(std::make_shared<Sexpr>(Sexpr::pair(car, cdr)))
+        {
+        }
 
-		List(const Sexpr &sexpr) : sexpr(std::make_shared<Sexpr>(sexpr)) {
-			if (!sexpr.is_pair()) {
-				throw SexprTypeException();
-			}
-		}
+        List(const Sexpr &sexpr) : sexpr(std::make_shared<Sexpr>(sexpr)) {
+            if (!sexpr.is_pair()) {
+                throw SexprTypeException();
+            }
+        }
 
-		List(Sexpr &&sexpr) : sexpr(std::make_shared<Sexpr>(std::forward<Sexpr>(sexpr))) {
-			if (!sexpr.is_pair()) {
-				throw SexprTypeException();
-			}
-		}
+        List(Sexpr &&sexpr) : sexpr(std::make_shared<Sexpr>(std::forward<Sexpr>(sexpr))) {
+            if (!sexpr.is_pair()) {
+                throw SexprTypeException();
+            }
+        }
 
-		List(Sexpr::ptr &sexpr) : sexpr(sexpr) {}
+        List(Sexpr::ptr &sexpr) : sexpr(sexpr) {}
 
         bool operator==(const List& rhs) const {
             return *sexpr == *rhs.sexpr;
         }
 
-		std::string to_text() const {
-			return sexpr->to_text();
-		}
+        std::string to_text() const {
+            return sexpr->to_text();
+        }
 
-		Sexpr::ptr first() const {
-			return sexpr->first();
-		}
+        Sexpr::ptr first() const {
+            return sexpr->first();
+        }
 
-		Sexpr::ptr rest() const {
-			return sexpr->rest();
-		}
+        Sexpr::ptr rest() const {
+            return sexpr->rest();
+        }
 
-		void set_first(Sexpr *first) {
-			sexpr->set_first(first);
-		}
+        void set_first(Sexpr *first) {
+            sexpr->set_first(first);
+        }
 
-		void set_rest(Sexpr *rest) {
-			sexpr->set_rest(rest);
-		}
+        void set_rest(Sexpr *rest) {
+            sexpr->set_rest(rest);
+        }
 
-	};
+    };
 
 
-	inline std::ostream& operator<<(std::ostream &out, const Sexpr &sexpr) {
-		return out << sexpr.to_text();
-	}
+    inline std::ostream& operator<<(std::ostream &out, const Sexpr &sexpr) {
+        return out << sexpr.to_text();
+    }
 
-	inline std::ostream& operator<<(std::ostream &out, const Sexpr::ptr &sexpr) {
-		return out << "(shared_ptr " << sexpr->to_text() << ")";
-	}
+    inline std::ostream& operator<<(std::ostream &out, const Sexpr::ptr &sexpr) {
+        return out << "(shared_ptr " << sexpr->to_text() << ")";
+    }
 
-	inline std::ostream& operator<<(std::ostream &out, const List &list) {
-		return out << list.to_text();
-	}
+    inline std::ostream& operator<<(std::ostream &out, const List &list) {
+        return out << list.to_text();
+    }
 
 }
--- src/sexpr.cpp
+++ src/sexpr.cpp
@@ -1,123 +1,126 @@
 #include <sxx/sexpr.hpp>
 
 namespace sxx {
-	const Sexpr Sexpr::empty = Sexpr();
-	const Sexpr::ptr List::null = std::make_shared<Sexpr>();
+    const Sexpr Sexpr::empty = Sexpr();
+    const Sexpr::ptr List::null = std::make_shared<Sexpr>();
 
     bool Sexpr::pair::operator==(const Sexpr::pair& rhs) const {
         return *car == *rhs.car && *cdr == *rhs.cdr;
     }
 
-	std::string Sexpr::to_text() const {
-		std::string text;
+    std::string Sexpr::to_text() const {
+        std::string text;
 
-		bool outer_sexpr = is_pair();
+        bool outer_sexpr = is_pair();
 
-		if (outer_sexpr) {
-			text = '(';
-		}
+        if (outer_sexpr) {
+            text = '(';
+        }
 
-		const Sexpr *current = this;
+        const Sexpr *current = this;
 
-		while (true) {
-			if (current->is_pair()) {
-				text += current->first()->to_text();
-				current = current->rest().get();
+        while (true) {
+            if (current->is_pair()) {
+                text += current->first()->to_text();
+                current = current->rest().get();
                 if (current->is_empty()) {
                     break;
                 }
                 else if (current->is_pair())
-					text += ' ';
-				else
-					text += " . ";
+                    text += ' ';
+                else
+                    text += " . ";
 
                 continue;
-			}
-			else if (current->is_bool()) {
-				if (current->as_bool())
-					text += "#t";
-				else
-					text += "#f";
-			}
-			else if (current->is_string()) {
-				text += "\"";
-				text += current->as_string();
-				text += "\"";
-			}
-			else if (current->is_symbol()) {
-				text += current->as_symbol().c_str();
             }
-			else if (current->value.type() == typeid(double)) {
-				text += std::to_string(any_cast<double>(current));
-			}
-			else if (current->value.type() == typeid(int64_t)) {
-				text += std::to_string(any_cast<int64_t>(current));
+            else if (current->is_bool()) {
+                if (current->as_bool())
+                    text += "#t";
+                else
+                    text += "#f";
+            }
+            else if (current->is_string()) {
+                text += "\"";
+                text += current->as_string();
+                text += "\"";
+            }
+            else if (current->is_symbol()) {
+                text += current->as_symbol().c_str();
+            }
+            else if (current->value.type() == typeid(double)) {
+                text += std::to_string(any_cast<double>(current));
+            }
+            else if (current->value.type() == typeid(int64_t)) {
+                text += std::to_string(any_cast<int64_t>(current));
             }
             else if (current->value.type() == typeid(void*)) {
-				text += "<pointer>";
-			}
+                text += "<pointer>";
+            }
             else {
                 text += "<unknown:";
                 text += current->value.type().name();
                 text += ">";
             }
             break;
-		}
+        }
 
-		if (outer_sexpr) {
-			text += ')';
-		}
+        if (outer_sexpr) {
+            text += ')';
+        }
 
-		return text;
-	}
+        return text;
+    }
 
     bool Sexpr::operator==(const Sexpr& rhs) const {
         if (is_pair()) {
             return rhs.is_pair() && as_pair() == rhs.as_pair();
-        } else if (is_empty()) {
+        }
+        else if (is_empty()) {
             return rhs.is_empty();
-        } else if (is_string()) {
+        }
+        else if (is_string()) {
             return rhs.is_string() && as_string() == rhs.as_string();
-        } else if (is_symbol()) {
+        }
+        else if (is_symbol()) {
             return rhs.is_symbol() && as_symbol() == rhs.as_symbol();
         }
 
         return rhs.value == value;
     }
 
-	Sexpr::ptr Sexpr::first() const {
-		try {
-			return any_cast<Sexpr::pair>(value).car;
-		}
-		catch (bad_any_cast &) {
-			throw new SexprTypeException();
-		}
-	}
+    Sexpr::ptr Sexpr::first() const {
+        try {
+            return any_cast<Sexpr::pair>(value).car;
+        }
+        catch (bad_any_cast &) {
+            throw new SexprTypeException();
+        }
+    }
 
-	Sexpr::ptr Sexpr::rest() const {
-		try {
-			return any_cast<Sexpr::pair>(value).cdr;
-		}
-		catch (bad_any_cast &) {
-			throw new SexprTypeException();
-		}
-	}
+    Sexpr::ptr Sexpr::rest() const {
+        try {
+            return any_cast<Sexpr::pair>(value).cdr;
+        }
+        catch (bad_any_cast &) {
+            throw new SexprTypeException();
+        }
+    }
 
-	void Sexpr::set_first(Sexpr *first) {
-		try {
-			return any_cast<Sexpr::pair>(value).car.reset(first);
-		}
-		catch (bad_any_cast &) {
-			throw new SexprTypeException();
-		}
-	}
+    void Sexpr::set_first(Sexpr *first) {
+        try {
+            return any_cast<Sexpr::pair>(value).car.reset(first);
+        }
+        catch (bad_any_cast &) {
+            throw new SexprTypeException();
+        }
+    }
 
-	void Sexpr::set_rest(Sexpr *rest) {
-		try {
-			return any_cast<Sexpr::pair>(value).cdr.reset(rest);
-		}
-		catch (bad_any_cast &) {
-			throw new SexprTypeException();
-		}
-	}
+    void Sexpr::set_rest(Sexpr *rest) {
+        try {
+            return any_cast<Sexpr::pair>(value).cdr.reset(rest);
+        }
+        catch (bad_any_cast &) {
+            throw new SexprTypeException();
+        }
+    }
 }
